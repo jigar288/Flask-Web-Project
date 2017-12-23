2aa3bd039148afd35a6d44911dd9fee10cde6886
Jigar Patel <jigar.moyo@gmail.com>
2017-12-22 20:46:44 -0600

    Add files via upload

---
+++ Flask Web Project/simple_app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name='Jigar'):
+    return "hello from {}".format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+#    in context the num1 on the left is from python parameter & on the right is for html page
+    context = {'num1': num1, 'num2': num2} 
+    return render_template('add.html', **context) 
+
+app.run(debug=True, port=8000, host='0.0.0.0')
+
+
+
+
